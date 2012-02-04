9997eb5e50f113f68609ffd1d4ffe35aec4a40ed
Gustavo Beathyate <goddamnhippie@me.com>
2012-02-04 10:42:56 -0500

    Actualizacion a Rails 3.2.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Guachiman::Application.config.secret_token = '61d3370b186f2d51a0aa1f60ef908958e08f1a10f929c013c429bb193d50413e0a6ab02877720f122f5c1b559acbccf80a986629be493d1c62c49b6a86257988'
+Guachiman::Application.config.secret_token = '34b4a974d65f0a88447f88630d7a08d566da39ee1d282f9e37399396e953eb6835742b46da96f75e4c27d9e362c79e977b911504255e4d78b5e657ea8a4f32b0'
