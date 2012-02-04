f8d4bed59959ef56ce37172548f63479008bb1f8
Gustavo Beathyate <goddamnhippie@me.com>
2012-02-04 11:10:07 -0500

    generar scaffold para el modelo server

---
+++ db/migrate/20120204154407_create_servers.rb
@@ -0,0 +1,11 @@
+class CreateServers < ActiveRecord::Migration
+  def change
+    create_table :servers do |t|
+      t.string :name
+      t.string :address
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
