de33642a6c25caf1703f05f1e0d714638c890a98
Gustavo Beathyate <goddamnhippie@me.com>
2012-02-07 10:20:52 -0500

    modelo servidor

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,22 @@
 class Server < ActiveRecord::Base
   
+  validates :name,    presence: true
+  validates :address, presence: true
+  validates :port,    presence: true, numericality: true
+
+  has_many :checks
+
+  def self.perform_checks!
+    all.each(&:check!)
+  end
+
+  def server_and_port
+    "#{address}:#{port}"
+  end
+
+  def check!
+    current_check = checks.create
+    current_check.perform!
+  end
+  
 end
